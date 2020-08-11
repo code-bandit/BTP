69fd968167195b8166c09933dce524cf65a2c958
code-bandit <45354471+code-bandit@users.noreply.github.com>
2020-08-11 15:00:46 +0530

    ppt notes

---
+++ code/isvm.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+""" Definitions """
+T = None # training dataset
+K = 3 # no. of classifications
+T_m = 1 # mth sample set
+
+""" Decision Function """
+def func(x):
+    pass
+
+""" Main Function """
+def main():
+    m = 1
+
+    # while m < k-1:
+        
+
+        
+        # m = m+1
+
+if __name__ == "__main__":
+    main()
